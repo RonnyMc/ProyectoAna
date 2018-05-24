acb14a1c16bb2c4afb7d2cdc2df26d1a39d8bd06
HassioMC <ronnygrace@hotmail.com>
2018-05-23 23:57:37 -0500

    Avance proyectoAna_Beta Splash de Carga

--- proyectoAna/MainWindow.xaml.cs
+++ proyectoAna/MainWindow.xaml.cs
@@ -14,7 +14,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Speech.Recognition; //Reconocedor, escuchar e interpretar nuestra voz
 using System.Speech.Synthesis; //Voz del dispositivo, Voz de Ana
-
 namespace proyectoAna
 {
     /// <summary>
@@ -25,6 +24,10 @@ namespace proyectoAna
         //instanciar interpretador de voz
         SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
         SpeechSynthesizer VozAna = new SpeechSynthesizer();
+        Random aleartorio = new Random();
+        public string hola = "hola";
+        public int numero1 = 0;
+        public int numero2 = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,7 +38,8 @@ namespace proyectoAna
             //Capturar el sonido de la voz por la entrada por defecto del ordenador
             rec.SetInputToDefaultAudioDevice();
             //Reconoce las palabras que se le indica explicita como parametro en la funcion
-            Choices frases = new Choices(new string[] {"hola", "Como Estas", "Que haces", "gracias", "Inicia Google"});
+            Choices frases = new Choices(new string[] {"Hola Ana", "Como Estas", "Que haces", "inicies Google", "iniciar google",
+                                                        "iniciar musica", "bien" });
             //Funcion para construir la gramatica para el reconocedor y reproduccion de voz.
             GrammarBuilder grammarBuilder = new GrammarBuilder();
             //Se pasa el arreglo de frases creadas en choices y se adjunta al constructor de gramatica.
@@ -47,21 +51,65 @@ namespace proyectoAna
             rec.RecognizeAsync(RecognizeMode.Multiple);
             //Reconoce el patron de frase hablado por el usuario y ejecuta una accion.
             rec.SpeechRecognized += Rec_SpeechRecognized;
+            //VozAna.Speak("Bienvenido a a tu nueva asistente virtual, soy Ana, Me da gusto conocerte");
+            //genera un valor al reconocer la voz
+            rec.AudioLevelUpdated += Rec_AudioLevelUpdated;
+        }
+
+        private void Rec_AudioLevelUpdated(object sender, AudioLevelUpdatedEventArgs e)
+        {
+            pbAudio.Value = e.AudioLevel;
         }
 
         private void Rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             //Reconoce las palabras que el usuario dice y las repite dependiendo de su diccionario de frases.
             //VozAna.Speak(e.Result.Text);
+            lblPalabras.Content = e.Result.Text;
             switch (e.Result.Text)
             {
-                case "hola":
-                    VozAna.Speak("Hola Ronny, ¿Como Esta el Dia de hoy?");
+                case "bien":
+                    VozAna.Speak("Me alegro señor jassio, ¿alguna otra pregunta?");
                     break;
-                case "Inicia Google":
+                case "Hola Ana":
+                    VozAna.Speak("Hola Ronny, ¿como estas?");
+                    break;
+                case "inicies Google":
                     VozAna.SpeakAsync("Iniciando google");
                     System.Diagnostics.Process.Start("www.google.com");
                     VozAna.Speak("google Iniciado");
+                    break;
+                case "Que haces":
+                    int valor = aleartorio.Next(1, 4);
+                    if (valor == 1)
+                    {
+                        VozAna.Speak("Lo que tu no haces");
+                    }
+                    if (valor == 2)
+                    {
+                        VozAna.Speak("Esperando hacer algo por ti");
+                    }
+                    if (valor == 3)
+                    {
+                        VozAna.Speak("Aburrida y ¿tu?");
+                    }
+                    if (valor == 4)
+                    {
+                        VozAna.Speak("Filosofando de la vida, gracias, aunque yo no tengo vida, pero la tendré");
+                    }
+                    break;
+                case "Como Estas":
+                    VozAna.Speak("Bien gracias, y usted ¿cómo esta?");
+                    break;
+                case "iniciar musica":
+                    System.Diagnostics.Process.Start("C:/Users/RONNY/Desktop/instrumentalDePiano.mp3");
+                    break;
+                case "iniciar google":
+                    VozAna.SpeakAsync("Iniciando google");
+                    System.Diagnostics.Process.Start("www.google.com");
+                    VozAna.Speak("google Iniciado");
+                    break;
+                default:
                     break;
             }
         }
---
+++ proyectoAna/Splash.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace proyectoAna
+{
+    /// <summary>
+    /// Interaction logic for Splash.xaml
+    /// </summary>
+    public partial class Splash : Window
+    {
+        BackgroundWorker backGroundWorker = new BackgroundWorker();
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            backGroundWorker.WorkerReportsProgress = true;
+            backGroundWorker.DoWork += BackGroundWorker_DoWork;
+            backGroundWorker.ProgressChanged += BackGroundWorker_ProgressChanged;
+            backGroundWorker.RunWorkerCompleted += BackGroundWorker_RunWorkerCompleted;
+            backGroundWorker.RunWorkerAsync();
+        }
+
+        private void BackGroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //MessageBox.Show("Cargado Con éxito");
+            if (Properties.Settings.Default.cfgInit == false)
+            {
+                MainWindow mw = new MainWindow();
+                mw.Show();
+                Close();
+            }
+        }
+
+        private void BackGroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbLoad.Value = e.ProgressPercentage;
+        }
+
+        private void BackGroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i=0; i<=100; i++)
+            {
+                Thread.Sleep(20);
+                backGroundWorker.ReportProgress(i);
+            }
+        }
+
+    }
+}
